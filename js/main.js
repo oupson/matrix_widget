2fa47c89f3ef058ed6c7acc3951956741fadcfa6
Oupson <oupson1er@gmail.com>
2020-07-20 22:06:52 +0200

    Add default image for group Join button

--- js/main.js
+++ js/main.js
@@ -7,6 +7,9 @@ document.addEventListener('DOMContentLoaded', async () => {
     var groupId = urlParams.get("groupId");
     debuglog("GroupId", groupId);
 
+    document.getElementById("join_button").onclick = () => {
+        window.open("https://matrix.to/#/" + groupId);
+    }
     if (urlParams.has("serverUrl")) {
         serverUrl = urlParams.get("serverUrl");
     }
@@ -39,7 +42,15 @@ async function getSummary(groupId, access_token) {
     })).json()
     debuglog("Summary", summary);
 
-    document.getElementById("icon").src = serverUrl + "_matrix/media/r0/download/" + summary.profile.avatar_url.replace("mxc://", "");
+    if (summary.profile.avatar_url != null && summary.profile.avatar_url != "") {
+        document.getElementById("icon").src = serverUrl + "_matrix/media/r0/download/" + summary.profile.avatar_url.replace("mxc://", "");
+    } else { 
+        let group_image_div = document.createElement("div")
+        group_image_div.classList.add("group_image_replacement");
+        group_image_div.innerHTML = "<p>" + summary.profile.name[0] + "</p>";
+        document.getElementById("icon").parentNode.replaceChild(group_image_div, document.getElementById("icon"));
+    }
+    
     document.getElementById("description").innerHTML = summary.profile.short_description;
     document.getElementById("long_description").innerHTML = summary.profile.long_description;
     document.getElementById("name").innerHTML = summary.profile.name;
@@ -85,11 +96,17 @@ async function getUsers(groupId, access_token) {
         let user_div = document.createElement("div");
         user_div.classList.add("user");
 
-        let user_image = document.createElement("img");
         if (user.avatar_url != null && user.avatar_url != "") {
+            let user_image = document.createElement("img");
+        
             user_image.src = serverUrl + "_matrix/media/r0/download/" + user.avatar_url.replace("mxc://", "");
+            user_div.appendChild(user_image);
+        } else {
+            let user_image_div = document.createElement("div")
+            user_image_div.classList.add("user_image_replacement");
+            user_image_div.innerHTML = "<p>" + ((user.displayname != "" && user.displayname != null) ? user.displayname : user.user_id)[0] + "</p>";
+            user_div.appendChild(user_image_div);
         }
-        user_div.appendChild(user_image);
 
         let user_name = document.createElement("p");
         user_name.innerHTML = (user.displayname != "" && user.displayname != null) ? user.displayname : user.user_id;
